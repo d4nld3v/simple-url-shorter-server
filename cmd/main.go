418b38c209d658355bfdaae8263d152bd277e791
d4nld3v <danielenriquezdeveloper@outlook.com>
2025-08-09 11:03:17 +0200

    feat: add functions to validate the raw url

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/d4nld3v/url-shortener-go/services"
 )
 
 func main() {
@@ -14,11 +16,19 @@ func main() {
 
 	url := os.Args[1]
 
+	// validar la url
+	valid, err := services.IsValidURL(url)
+	if err != nil {
+		fmt.Println("Error validating URL:", err)
+		return
+	}
+
+	if valid {
+		fmt.Println("URL is valid")
+	}
+	//generar una id unica para la url ( acortar la url )
+	//almacenar la url original y la id unica en una base de datos
+	//redirigir a la url original cuando se accede a la id unica
+
 	fmt.Println("Original URL:", url)
 }
-
-// funcion para acortar la url original
-// generar una id unica mas corta para la url
-// almacenar la url original y la id unica en una base de datos
-// redirigir a la url original cuando se accede a la id unica
-// funcion para consultar la url original a partir de la id unica
--- services/validator.go
+++ services/validator.go
@@ -1,6 +1,76 @@
 package services
 
-func IsValidURL(url string) bool {
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+func IsValidURL(raw string) (bool, error) {
+
+	if len(raw) > 2048 {
+		return false, fmt.Errorf("url is too long")
+	}
+
+	u, err := url.ParseRequestURI(raw)
+
+	if err != nil {
+		return false, fmt.Errorf("url is null: %w", err)
+	}
+
+	if u.Scheme == "" || u.Host == "" {
+		return false, fmt.Errorf("url not has scheme or host")
+	}
+
+	if !isHttpURL(u) {
+		return false, fmt.Errorf("url is not http or https")
+	}
+
+	if !isPublicIP(u) {
+		return false, fmt.Errorf("url is not public IP")
+	}
+
+	if !isAvailable(u) {
+		return false, fmt.Errorf("url is not available: %w", err)
+	}
+
+	return true, nil
+}
+
+func isHttpURL(u *url.URL) bool {
+	if u.Scheme != "http" && u.Scheme != "https" {
+		return false
+	}
 
 	return true
+}
+
+func isPublicIP(u *url.URL) bool {
+
+	ip := net.ParseIP(u.Hostname())
+	if ip == nil {
+		return false
+	}
+
+	if ip.IsLoopback() || ip.IsPrivate() || ip.IsUnspecified() {
+		return false
+	}
+
+	return true
+}
+
+func isAvailable(u *url.URL) bool {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Head(u.String())
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode >= 200 && resp.StatusCode < 400
 }
